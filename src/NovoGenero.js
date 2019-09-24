128c8f483ff5160b211db1e313bd701b0637b13f
Edivaldo Junior <edivaldo.junior@wexinc.com>
2019-09-24 17:15:08 -0300

    first commit

---
+++ src/NovoGenero.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+import axios from 'axios'
+import {Redirect} from 'react-router-dom'
+
+const NovoGenero = () => {
+  const [name, setName] = useState('')
+  const [success, setSuccess] = useState(false)
+  const onChange = evt => {
+    //console.log(evt.target.value);
+    setName(evt.target.value) 
+  }
+  const save = () => {
+    axios.post('/api/genres', {
+      name
+    })
+    .then(res => {
+      console.log(res);
+      setSuccess(true);
+    })
+  }
+  if (success) {
+    return <Redirect to='/generos' />
+  }
+  return (
+    <div className='container'>
+      <h1>Novo Gênero</h1>
+      <form>
+        <div className='form-group'>
+          <label htmlFor='name'>Nome</label>
+          <input type='text' className='form-control' id='name' placeholder='Nome do Gênero' 
+            value={name} onChange={onChange} />
+        </div>
+        <button type='button' onClick={save} className='btn btn-primary'>Salvar</button>
+      </form>
+    </div>
+  )
+}
+
+export default NovoGenero
