7619e76a3aa243fbf9f938790998ea80ef18fddd
Edivaldo Junior <edivaldo.junior@wexinc.com>
2019-09-27 13:26:09 -0300

    adding serie pages

---
+++ src/InfoSerie.js
@@ -0,0 +1,139 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import {Redirect} from 'react-router-dom'
+import { Badge } from 'reactstrap'
+
+const InfoSerie = ({ match }) => {
+  const [form, setForm] = useState({})
+  const [success, setSuccess] = useState(false)
+  const [mode, setMode] = useState('EDIT')  
+  const [data, setData] = useState({})
+  const [genres, setGenres] = useState([])
+  const [genreId, setGenreId] = useState('')
+
+  useEffect(() => {
+    axios
+      .get('/api/series/' + match.params.id)
+      .then(res => {
+        console.log(res)
+        setData(res.data)
+        setForm(res.data)
+      })
+  }, [match.params.id])
+
+  useEffect(() => {
+    axios
+      .get('/api/genres')
+      .then(res => {
+        setGenres(res.data.data)
+        const genres = res.data.data
+        const encontrado = genres.find(value => data.genre === value.name) 
+        if (encontrado) {
+          setGenreId(encontrado.id)
+        }
+      })
+  }, [data])
+
+  //custom header
+  const masterHeader = {
+    height: '50vh',
+    minHeight: '500px',
+    backgroundImage: `url('${data.background}')`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    backgroundRepeat: 'no-repeat'
+  }
+
+  const onChange = field => evt => {
+    //console.log(evt.target.value);
+    setForm({
+      ...form,
+      [field]: evt.target.value
+    }) 
+  }
+
+  const seleciona = value => () => {
+    setForm( {
+      ...form,
+      status: value
+    })
+  }
+  const save = () => {
+    axios.put('/api/series/' + match.params.id, {
+      ...form,
+      genre_id: genreId
+    })
+    .then(res => {
+      setSuccess(true);
+    })
+  }
+  if (success) {
+    //return <Redirect to='/series' />
+  }
+  return (
+    <div>
+      <header style={masterHeader}>
+        <div className='h-100' style={{ background: 'rgba(0,0,0,0.7)'}}>
+          <div className='h-100 container'>
+            <div className='row h-100 align-items-center'>
+              <div className='col-3'>
+                <img alt={data.name} className='img-fluid img-thumbnail' src={data.poster} />
+              </div>
+              <div className='col-8'>
+                <h1 className='font-weight-light text-white'> {data.name}</h1>
+                <div className='lead text-white'>
+                  { data.status ==='ASSISTIDO' && <Badge color='success'>Assistido</Badge> }
+                  { data.status === 'PARA_ASSISTIR' && <Badge color='warning'>Para assistir</Badge> }
+                  Gênero: {data.genre}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
+      <div>
+        <button className='btn btn-primary' onClick={() => setMode('EDIT')}>Editar</button>
+      </div>
+      {
+        mode === 'EDIT' &&
+        <div className='container'>
+          <h1>Nova Série</h1>
+          <pre>{JSON.stringify(form)}</pre>
+          <button  className='btn btn-primary' onClick={() => setMode('INFO')}>Cancelar edição</button>
+          <form>
+            <div className='form-group'>
+              <label htmlFor='name'>Nome</label>
+              <input type='text' className='form-control' id='name' placeholder='Nome da Série' 
+                value={form.name} onChange={onChange('name')} />
+            </div>
+            <div className='form-group'>
+              <label htmlFor='comments'>Comentários</label>
+              <input type='text' className='form-control' id='comments' placeholder='Comentários'
+                value={form.comments} onChange={onChange('comments')} />
+            </div>
+            <div className='form-group'>
+              <label htmlFor='genre'>Gênero</label>
+              <select className='form-control' id='genre' onChange={onChange('genre_id')} value={genreId}>
+                { genres.map(genre => <option key={genre.id} value={genre.id}
+                  selected={genre.id === form.genre}>{genre.name}</option>) }
+              </select>
+            </div>
+            <div className='form-check'>
+              <input className='form-check-input' type='radio' name='status' id='assistido' value='ASSISTIDO' 
+                onClick={seleciona('ASSISTIDO')} />
+              <label className='form-check-label' htmlFor='assistido'>Assistido</label>
+            </div>
+            <div className='form-check'>
+              <input className='form-check-input' type='radio' name='status' id='paraAssistir' value='PARA_ASSISTIR' 
+                onClick={seleciona('PARA_ASSISTIR')} />
+              <label className='form-check-label' htmlFor='paraAssistir'>Para assistir</label>
+            </div>
+            <button type='button' onClick={save} className='btn btn-primary'>Salvar</button>
+          </form>
+        </div>
+      }
+    </div>
+  )
+}
+
+export default InfoSerie
--- src/Series.js
+++ src/Series.js
@@ -2,19 +2,19 @@ import React, {useState, useEffect} from 'react'
 import axios from 'axios'
 import { Link } from 'react-router-dom'
 
-const Generos = () => {
+const Series = () => {
   const [data, setData] = useState([])
   useEffect(() => {
     axios
-    .get('/api/genres')
+    .get('/api/series')
     .then(res => {
       setData(res.data.data)
     })
   }, [])
 
-  const deleteGenero = id  => {
+  const deleteSerie = id  => {
     axios
-      .delete('/api/genres/' + id)
+      .delete('/api/series/' + id)
       .then(res => {
         setData(data.filter(item => item.id !== id))
       })
@@ -25,8 +25,8 @@ const Generos = () => {
         <th scope='row'>{record.id}</th>
           <td>{record.name}</td>
           <td>
-            <button className='btn btn-danger' onClick={ () => deleteGenero(record.id) }>Remover</button>
-          <Link to={'/generos/' + record.id} className='btn btn-warning'>Editar</Link>
+          <button className='btn btn-danger' onClick={() => deleteSerie(record.id) }>Remover</button>
+          <Link to={'/series/' + record.id} className='btn btn-warning'>Editar</Link>
           </td>
       </tr>
     )
@@ -35,21 +35,21 @@ const Generos = () => {
   if (data.length === 0) {
     return(
       <div className='container'>
-        <h1>Gêneros</h1>
+        <h1>Séries</h1>
         <div>
-          <Link to='/generos/novo' className='btn btn-primary'>Novo Gênero</Link>
+          <Link to='/series/novo' className='btn btn-primary'>Nova Séries</Link>
         </div>
         <div className='alert alert-warning' role='alert'>
-          Você não possui Gêneros criados.
+          Você não possui séries criadas.
         </div>
       </div>
     )
   }
   return (
     <div className='container'>
-      <h1>Gêneros</h1>
+      <h1>Séries</h1>
       <div>
-        <Link to='/generos/novo' className='btn btn-primary'>Novo Gênero</Link>
+        <Link to='/series/novo' className='btn btn-primary'>Nova Séries</Link>
       </div>
       <table className='table table-dark'>
         <thead>
@@ -66,4 +66,4 @@ const Generos = () => {
     </div>
   )
 }
-export default Generos
+export default Series
